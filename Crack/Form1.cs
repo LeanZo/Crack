fe470f97b598b9e070615a718c72f9a05d48421d
LeanZo <kashchannel@gmail.com>
2017-02-24 22:11:00 -0300

    BASE
    app base

---
+++ Crack/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Crack
+{
+    public partial class Form1 : Form
+    {
+        int numbers = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string crackpassstring = Convert.ToString(numbers);
+            numbers += 1;
+            SendKeys.Send(crackpassstring);
+            SendKeys.Send("{Enter}");
+            SendKeys.Send("{Enter}");
+        }
+    }
+    }
